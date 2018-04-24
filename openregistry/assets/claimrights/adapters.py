87de892ac2d6c92964837a52a74f99dcc5ff2436
Oleksiy Veretiuk <oleksiy.veretiuk@gmail.com>
2018-04-24 09:42:05 +0300

    ClaimRight asset manager

--- openregistry/assets/claimrights/adapters.py
+++ openregistry/assets/claimrights/adapters.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from openregistry.assets.core.adapters import AssetConfigurator
+from openregistry.assets.core.adapters import AssetConfigurator, AssetManagerAdapter
 from openregistry.assets.core.constants import STATUS_CHANGES
 
 
@@ -8,3 +8,15 @@ class ClaimRightsAssetConfigurator(AssetConfigurator):
 
     name = "ClaimRights Asset configurator"
     available_statuses = STATUS_CHANGES
+
+
+class ClaimRightsAssetManagerAdapter(AssetManagerAdapter):
+    name = "Asset Manager for claimrights asset"
+    context = None
+    create_validation = []
+
+    def __init__(self, context):
+        self.context = context
+
+    def create_asset(self, request):
+        self._validate(request, self.create_validation)
--- openregistry/assets/claimrights/includeme.py
+++ openregistry/assets/claimrights/includeme.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 from pyramid.interfaces import IRequest
 from openregistry.assets.core.includeme import IContentConfigurator
+from openregistry.assets.core.interfaces import IAssetManager
 from openregistry.assets.claimrights.models import Asset, IClaimRightsAsset
-from openregistry.assets.claimrights.adapters import ClaimRightsAssetConfigurator
+from openregistry.assets.claimrights.adapters import ClaimRightsAssetConfigurator, ClaimRightsAssetManagerAdapter
 
 
 def includeme(config, plugin_config=None):
@@ -12,3 +13,6 @@ def includeme(config, plugin_config=None):
     config.registry.registerAdapter(ClaimRightsAssetConfigurator,
                                     (IClaimRightsAsset, IRequest),
                                     IContentConfigurator)
+    config.registry.registerAdapter(ClaimRightsAssetManagerAdapter,
+                                    (IClaimRightsAsset, ),
+                                    IAssetManager)
